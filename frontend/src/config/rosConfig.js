1fe44e8dcc6a32016215db9ee3c01ffc835c87ce
albihan <ridoalbihan@gmail.com>
2026-01-26 19:55:16 +0700

    Update ROS websocket config and Topics

--- frontend/src/config/rosConfig.js
+++ frontend/src/config/rosConfig.js
@@ -1,22 +1,11 @@
 export const ROS_CONFIG = {
     WEBSOCKET_URL: `ws://${window.location.hostname}:9090`,
     TOPICS: {
-        // topic dummy dari script python
-        DATA_ENGINEER:{
-            name: '/dashboard/telemetry',
-            type: 'std_msgs/String'
-        },
-        IOT_SENSOR:{
-            name: '/iot_data',
-            type: 'std_msgs/String'
-        },
-        AI_DETECTION:{
-            name: '/ai_data',
-            type: 'std_msgs/String'
-        },
-        CONFIG_CMD:{
-            name: '/dashboard/telemetry',
-            type: 'std_msgs/String'
-        }
+        SUHU: { name: '/suhu', type: 'std_msgs/String' },
+        KELEMBAPAN: { name: '/kelembapan', type: 'std_msgs/String' },
+        LDR: { name: '/ldr', type: 'std_msgs/String' },
+        // asumsi topik yg dikirim dari tim AI
+        AI: { name: '/plant_health_status', type: 'std_msgs/String' }, // Asumsi output Sistem Cerdas
+        CONFIG: { name: 'dashboard_config', type: 'std_msgs/String' }
     }
 };
